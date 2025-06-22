2f9b8a2abb4810cfb6703b682fe6e1d98a5b8a34
Regis Freyd <djaiss@users.noreply.github.com>
2025-06-21 21:26:43 -0400

    feat: improve icons (#5)

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -7,8 +7,5 @@
 <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}" />
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}" />
 
-<!-- Safari pinned tab -->
-<link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#5bbad5" />
-
 <title>{{ config('app.name') }}</title>
 <meta name="description" content="{{ config('organizationos.description') }}" />
