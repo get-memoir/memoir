7d7b3191db4f32024fd1d93c4b5b35a4637bb44b
Regis Freyd <djaiss@users.noreply.github.com>
2025-09-09 07:00:03 -0400

    chore: add seeder for job families (#25)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Actions\CreateAccount;
+use App\Actions\CreateJobFamily;
 use App\Models\User;
+use App\Models\Organization;
 use Illuminate\Database\Seeder;
 use App\Actions\CreateOrganization;
 
 final class DatabaseSeeder extends Seeder
 {
     private User $michael;
+
+    private Organization $organization;
 
     /**
      * Seed the application's database.
@@ -20,7 +24,8 @@ final class DatabaseSeeder extends Seeder
     {
         $this->createDunderMifflin();
         $this->validateEmail();
-        $this->createOrganization();
+        $this->addOrganization();
+        $this->createJobFamilies();
     }
 
     private function createDunderMifflin(): void
@@ -39,11 +44,32 @@ final class DatabaseSeeder extends Seeder
         $this->michael->save();
     }
 
-    private function createOrganization(): void
+    private function addOrganization(): void
     {
-        new CreateOrganization(
+        $this->organization = new CreateOrganization(
             user: $this->michael,
             organizationName: 'Dunder Mifflin',
         )->execute();
+    }
+
+    private function createJobFamilies(): void
+    {
+        $jobFamilies = [
+            'Engineering' => 'Engineering job family',
+            'Design' => 'Design job family',
+            'Product' => 'Product job family',
+            'Marketing' => 'Marketing job family',
+            'Sales' => 'Sales job family',
+            'HR' => 'HR job family',
+            'Finance' => 'Finance job family',
+        ];
+        foreach ($jobFamilies as $name => $description) {
+            (new CreateJobFamily(
+                organization: $this->organization,
+                user: $this->michael,
+                jobFamilyName: $name,
+                description: $description,
+            ))->execute();
+        }
     }
 }
