a3f287d50d2f80d3a7b5e61e15c7bbd64be345ad
Regis Freyd <djaiss@users.noreply.github.com>
2025-09-16 06:43:17 -0400

    Update company-header.blade.php

--- resources/views/marketing/company/partials/company-header.blade.php
+++ resources/views/marketing/company/partials/company-header.blade.php
@@ -1,7 +1,7 @@
 <div class="border-b border-gray-200 py-3 text-sm">
   <div class="mx-auto flex max-w-7xl items-center justify-center gap-x-4 px-6 lg:px-8 xl:px-0">
     <a href="{{ route('marketing.company.index') }}" class="{{ request()->routeIs('marketing.company.index') ? 'border-b-3 border-blue-400' : 'border-b-3 border-transparent' }} group flex cursor-pointer flex-col items-center justify-center gap-x-2 gap-y-1 rounded-sm border px-2 py-1 transition-colors duration-150 hover:border-gray-400 hover:bg-white">
-      <x-phosphor-store class="h-4 w-4 text-gray-500" />
+      <x-phosphor-storefront class="h-4 w-4 text-gray-500" />
       <span class="text-gray-600">About</span>
     </a>
     <a href="{{ route('marketing.company.handbook.index') }}" class="{{ request()->routeIs('marketing.company.handbook.index') ? 'border-b-3 border-blue-400' : 'border-b-3 border-transparent' }} group flex cursor-pointer flex-col items-center justify-center gap-x-2 gap-y-1 rounded-sm border px-2 py-1 transition-colors duration-150 hover:border-gray-400 hover:bg-white">
